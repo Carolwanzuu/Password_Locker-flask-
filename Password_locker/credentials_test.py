d0ad085edc66a974434a2fbae3e8638de028cf7d
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-31 21:07:49 +0300

    imported unittest and credential class

---
+++ Password_locker/credentials_test.py
@@ -0,0 +1,8 @@
+import unittest
+from credentials import Credential
+
+class TestCredential(unittest.TestCase):
+    '''
+    test class that defines test cases for the credential class behaviors
+    '''
+    
