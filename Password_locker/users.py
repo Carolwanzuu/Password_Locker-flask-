91aab308e5c41e9f2049c9b0a54159080b5e98a7
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-31 11:51:46 +0300

    modified the user class

--- Password_locker/users.py
+++ Password_locker/users.py
@@ -5,19 +5,19 @@ class User:
 
     user_list = [] #empty user list
 
-def __init__(self, first_name,last_name,phone_number,email):
+    def __init__(self, first_name,last_name,phone_number,email):
 
-    '''
-    __init__method helps define properties for our objects
+        '''
+        __init__method helps define properties for our objects
     
-    Args:
-        first_name:New user first name.
-        last_name:New user last name.
-        phone_number:New user phone number.
-        email:New user email address.
-    '''
+        Args:
+            first_name:New user first name.
+            last_name:New user last name.
+            phone_number:New user phone number.
+            email:New user email address.
+        '''
 
-    self.first_name = first_name
-    self.last_name = last_name
-    self.phone_number = phone_number
-    self.email = email
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
