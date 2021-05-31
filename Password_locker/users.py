064694db7f89191d9594cb30456707e48c6585a2
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-31 11:38:58 +0300

    created 4 instance variables

--- Password_locker/users.py
+++ Password_locker/users.py
@@ -16,3 +16,8 @@ def __init__(self, first_name,last_name,phone_number,email):
         phone_number:New user phone number.
         email:New user email address.
     '''
+
+    self.first_name = first_name
+    self.last_name = last_name
+    self.phone_number = phone_number
+    self.email = email
