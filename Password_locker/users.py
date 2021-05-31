66dd9998b52c32e8f4fd80e16ff332d3587046ac
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-31 11:20:03 +0300

    created a constructor and passed in 5 arguments

--- Password_locker/users.py
+++ Password_locker/users.py
@@ -4,3 +4,15 @@ class User:
     """
 
     pass
+
+def __init__(self, first_name,last_name,phone_number,email):
+
+    '''
+    __init__method helps define properties for our objects
+    
+    Args:
+        first_name:New user first name.
+        last_name:New user last name.
+        phone_number:New user phone number.
+        email:New user email address.
+    '''
