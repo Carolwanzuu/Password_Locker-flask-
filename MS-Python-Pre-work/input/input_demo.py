43c55305f2a3c20bbf8d7c76138181aaa8b25e7c
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-31 10:31:25 +0300

    initialized repo

---
+++ MS-Python-Pre-work/input/input_demo.py
@@ -0,0 +1,39 @@
+# print("What is your name?")
+# name = input()
+
+# print("how old are you?")
+# age = input()
+
+# print(name)
+# print(age)
+
+# import sys
+
+# name = sys.argv[1]
+# age = sys.argv[2]
+
+# print(name)
+# print(age)
+
+# height = 45
+# if height>70:
+#     print("You are really tall")
+# elif height >50:
+#     print("You look great")
+# else:
+#     print("You are short")
+
+# numbers = [1,2,3,4,5]
+
+# for number in numbers:
+#     print(number)
+
+# name = "Carol"
+# age = 20
+
+# print(f"My name is {name} and I am {age} years old.")
+
+greetings = 'Hello, Moringa!'
+
+part_one = greetings[-8:-2]
+print(part_one)
