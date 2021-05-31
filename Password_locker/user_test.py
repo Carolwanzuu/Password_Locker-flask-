e7f42ab59644f6c8e5ce80ea7dc05eb9294a0943
Carolwanzuu <carolkyalo01@gmail.com>
2021-05-31 11:58:40 +0300

    imported unittest and user class

---
+++ Password_locker/user_test.py
@@ -0,0 +1,12 @@
+import unittest
+from unittest.case import TestCase
+from users import User
+
+class TestUser(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for user class behaviors
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''
